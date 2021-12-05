f488fd100ffe3fdb76a4f4f4e431c9e8f127f922
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-12-04 22:51:01 -0300

    Isolate SUT creation

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,5 +1,5 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 class FacebookApi {
   private readonly baseUrl = 'https://graph.facebook.com'
@@ -33,13 +33,22 @@ namespace HttpGetClient {
 }
 
 describe('FacebookApi', () => {
-  const clientId = 'any_client_id'
-  const clientSecret = 'any_client_secret'
+  let clientId: string
+  let clientSecret: string
+  let httpClient: MockProxy<HttpGetClient>
+  let suit: FacebookApi
+
+  beforeAll(() => {
+    clientId = 'any_client_id'
+    clientSecret = 'any_client_secret'
+    httpClient = mock()
+  })
+
+  beforeEach(() => {
+    suit = new FacebookApi(httpClient, clientId, clientSecret)
+  })
 
   it('Should get app token', async () => {
-    const httpClient = mock<HttpGetClient>()
-    const suit = new FacebookApi(httpClient, clientId, clientSecret)
-
     await suit.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
