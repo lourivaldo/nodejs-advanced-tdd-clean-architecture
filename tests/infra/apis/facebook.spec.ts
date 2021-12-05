59d060af06119f53453acc41fb354dcd49744cef
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-12-05 10:10:31 -0300

    Ensure FacebookApi gets user info

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,7 +15,9 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
     suit = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -40,6 +42,18 @@ describe('FacebookApi', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  it('Should get user info', async () => {
+    await suit.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
