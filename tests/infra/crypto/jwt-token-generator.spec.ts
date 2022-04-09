237f0088225c5f5922c4f6975f5be0abb87a9a31
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-09 19:48:12 -0300

    ensure JwtTokenGenerator rethrows if sign throws

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -36,4 +36,12 @@ describe('JwtTokenGenerator', () => {
 
     expect(token).toBe('any_token')
   })
+
+  it('Should rethrow if sign throw', async () => {
+    fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
+
+    const promise = sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+
+    await expect(promise).rejects.toThrow(new Error('token_error'))
+  })
 })
