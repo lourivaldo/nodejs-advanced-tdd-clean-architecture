03d325f2b37e80b3459d6a3bf78d355fd033b0a8
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-10 22:45:05 -0300

    ensure PgUserAccountRepository update account if id is defined

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -51,5 +51,28 @@ describe('PgUserAccountRepository', () => {
 
       expect(pgUser?.id).toBe(1)
     })
+
+    it('should update an account if id is defined', async () => {
+      await pgUserRepo.save({
+        email: 'any_email',
+        name: 'any_name',
+        facebookId: 'any_fb_id'
+      })
+
+      await sut.saveWithFacebook({
+        id: '1',
+        email: 'new_email',
+        name: 'new_name',
+        facebookId: 'new_fb_id'
+      })
+      const pgUser = await pgUserRepo.findOne({ where: { id: 1 } })
+
+      expect(pgUser).toEqual({
+        id: 1,
+        email: 'any_email',
+        name: 'new_name',
+        facebookId: 'new_fb_id'
+      })
+    })
   })
 })
