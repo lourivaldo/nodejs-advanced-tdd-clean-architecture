8255b6c2ae0c993ee979df0f90614f241057251f
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-10 22:39:40 -0300

    ensure PgUserAccountRepository create an account if id is undefined

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm'
 import { LoadUserAccountRepository } from '@/data/contracts/repositories'
 import { PgUser } from '@/infra/postgres/entities'
+import { SaveFacebookAccountRepository } from '../../../data/contracts/repositories/user-account'
 
 export class PgUserAccountRepository implements LoadUserAccountRepository {
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
@@ -12,5 +13,14 @@ export class PgUserAccountRepository implements LoadUserAccountRepository {
         name: pgUser.name ?? undefined
       }
     }
+  }
+
+  async saveWithFacebook (params: SaveFacebookAccountRepository.Params): Promise<void> {
+    const pgUserRepo = getRepository(PgUser)
+    await pgUserRepo.save({
+      email: params.email,
+      name: params.name,
+      facebookId: params.facebookId
+    })
   }
 }
