6a084846db408eb8b6bcc85c099de4a2c0809ba6
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-15 11:10:46 -0300

    ensure FacebookLoginController returns 200 if authentication succeeds

--- tests/application/facebook-login.spec.ts
+++ tests/application/facebook-login.spec.ts
@@ -1,6 +1,7 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 import { FacebookAuthentication } from '@/domain/features/facebook-authentication'
 import { AuthenticationError } from '../../src/domain/errors/authentication'
+import { AccessToken } from '@/domain/models'
 
 class FacebookLoginController {
   constructor (private readonly facebookAuth: FacebookAuthentication) { }
@@ -13,6 +14,14 @@ class FacebookLoginController {
       }
     }
     const result = await this.facebookAuth.perform({ token: httpRequest.token })
+    if (result instanceof AccessToken) {
+      return {
+        statusCode: 200,
+        data: {
+          accessToken: result.value
+        }
+      }
+    }
     return {
       statusCode: 401,
       data: result
@@ -31,6 +40,7 @@ describe('FacebookLoginController', () => {
 
   beforeAll(() => {
     facebookAuth = mock()
+    facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
   })
 
   beforeEach(() => {
@@ -78,6 +88,17 @@ describe('FacebookLoginController', () => {
     expect(httpResponse).toEqual({
       statusCode: 401,
       data: new AuthenticationError()
+    })
+  })
+
+  it('Should return 200 if authentication succeeds', async () => {
+    const httpResponse = await sut.handle({ token: 'any_token' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: {
+        accessToken: 'any_value'
+      }
     })
   })
 })
