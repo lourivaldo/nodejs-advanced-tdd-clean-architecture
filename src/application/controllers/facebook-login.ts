5a307842b061e7f68335dbc88c7fdb7c23e703b3
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-15 19:41:57 -0300

    apply ok helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,8 +1,8 @@
 import { RequiredFieldError } from '@/application/errors'
-import { badRequest, HttpResponse, unathorized } from '@/application/helpers'
+import { badRequest, HttpResponse, unathorized, ok } from '@/application/helpers'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { serverError } from '../helpers/http'
+import { serverError } from '@/application/helpers/http'
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuth: FacebookAuthentication) { }
@@ -14,12 +14,9 @@ export class FacebookLoginController {
       }
       const accessToken = await this.facebookAuth.perform({ token: httpRequest.token })
       if (accessToken instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: accessToken.value
-          }
-        }
+        return ok({
+          accessToken: accessToken.value
+        })
       }
       return unathorized()
     } catch (error: any) {
