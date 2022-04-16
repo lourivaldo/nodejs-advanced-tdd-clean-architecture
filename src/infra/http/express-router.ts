ed54a62bdd104ecc3a8b0449cfc171bf58f026a8
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-16 19:36:19 -0300

    move ExpressRouter to its own file

---
+++ src/infra/http/express-router.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { Controller } from '@/application/controllers'
+
+export class ExpressRouter {
+  constructor (private readonly controller: Controller) {}
+
+  async adapt (req: Request, res: Response): Promise<void> {
+    const httpResponse = await this.controller.handle({ ...req.body })
+    if (httpResponse.statusCode === 200) {
+      res.status(200).json(httpResponse.data)
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
+    }
+  }
+}
--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -2,19 +2,7 @@ import { Request, Response } from 'express'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { Controller } from '@/application/controllers'
 import { mock, MockProxy } from 'jest-mock-extended'
-
-class ExpressRouter {
-  constructor (private readonly controller: Controller) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body })
-    if (httpResponse.statusCode === 200) {
-      res.status(200).json(httpResponse.data)
-    } else {
-      res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
-    }
-  }
-}
+import { ExpressRouter } from '@/infra/http'
 
 describe('ExpressRouter', () => {
   let req: Request
