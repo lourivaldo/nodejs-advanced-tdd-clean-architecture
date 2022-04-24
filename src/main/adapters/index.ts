7306aa339bc1edd1a9527524f0cd25c7ac4154a2
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-04-23 21:43:22 -0300

    move ExpressMiddleware to its own file

--- src/main/adapters/index.ts
+++ src/main/adapters/index.ts
@@ -1,1 +1,2 @@
+export * from './express-middleware'
 export * from './express-router'
--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -1,27 +1,8 @@
-import { HttpResponse } from '@/application/helpers'
-import { getMockReq, getMockRes } from '@jest-mock/express'
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { mock, MockProxy } from 'jest-mock-extended'
-
-type Adapter = (middleware: Middleware) => RequestHandler
-
-const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
-  const { statusCode, data } = await middleware.handle({ ...req.headers })
-  if (statusCode === 200) {
-    const entries = Object.entries(data).filter(entry => entry[1])
-    req.locals = {
-      ...req.locals,
-      ...Object.fromEntries(entries)
-    }
-    next()
-  } else {
-    res.status(statusCode).json(data)
-  }
-}
-
-interface Middleware {
-  handle: (httpRequest: any) => Promise<HttpResponse>
-}
+import { Middleware } from '@/application/middlewares'
+import { adaptExpressMiddleware } from '@/main/adapters'
+import { getMockReq, getMockRes } from '@jest-mock/express'
 
 describe('ExpressMiddleware', () => {
   let req: Request
