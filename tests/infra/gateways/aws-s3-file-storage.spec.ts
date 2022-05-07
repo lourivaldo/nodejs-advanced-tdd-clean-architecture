801a48fd3a3f341dd36d2f40a8843f74ecba4e2e
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-07 18:59:18 -0300

    ensure AwsS3FileStorage calls putObject with correct input

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,11 +1,14 @@
-import { config } from 'aws-sdk'
+import { UploadFile } from '@/domain/contracts/gateways'
+import { config, S3 } from 'aws-sdk'
+import { mocked } from 'ts-jest/utils'
 
 jest.mock('aws-sdk')
 
 class AwsS3FileStorage {
   constructor (
-    private readonly accessKey: string,
-    private readonly secret: string
+    accessKey: string,
+    secret: string,
+    private readonly bucket: string
   ) {
     config.update({
       credentials: {
@@ -14,20 +17,43 @@ class AwsS3FileStorage {
       }
     })
   }
+
+  async upload (input: UploadFile.Input): Promise<void> {
+    const s3 = new S3()
+    await s3.putObject({
+      Bucket: this.bucket,
+      Key: input.key,
+      Body: input.file,
+      ACL: 'public-read'
+    }).promise()
+  }
 }
 
 describe('AwsS3FileStorage', () => {
   let sut: AwsS3FileStorage
   let accessKey: string
   let secret: string
+  let bucket: string
+  let key: string
+  let file: Buffer
+  let putObjectPromiseSpy: jest.Mock
+  let putObjectSpy: jest.Mock
 
   beforeAll(() => {
     accessKey = 'any_access_key'
     secret = 'any_secret'
+    bucket = 'any_bucket'
+    key = 'any_key'
+    file = Buffer.from('any_buffer')
+    putObjectPromiseSpy = jest.fn()
+    putObjectSpy = jest.fn().mockImplementationOnce(() => ({ promise: putObjectPromiseSpy }))
+    mocked(S3).mockImplementationOnce(jest.fn().mockImplementationOnce(() => ({
+      putObject: putObjectSpy
+    })))
   })
 
   beforeEach(() => {
-    sut = new AwsS3FileStorage(accessKey, secret)
+    sut = new AwsS3FileStorage(accessKey, secret, bucket)
   })
 
   it('should config aws credentials on creation', () => {
@@ -41,14 +67,16 @@ describe('AwsS3FileStorage', () => {
     expect(config.update).toHaveBeenCalledTimes(1)
   })
 
-  it('should config aws credentials on creation', () => {
-    expect(sut).toBeDefined()
-    expect(config.update).toHaveBeenCalledWith({
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secret
-      }
+  it('should call putObject with correct input', async () => {
+    await sut.upload({ file, key })
+
+    expect(putObjectSpy).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
     })
-    expect(config.update).toHaveBeenCalledTimes(1)
+    expect(putObjectSpy).toHaveBeenCalledTimes(1)
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
   })
 })
