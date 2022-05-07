04e7588cf6ca8521176cc98ae5ae2891806d6dc7
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-06 22:38:22 -0300

    ensure UniqueId return correct uuid

--- src/infra/gateways/uuid-handler.ts
+++ src/infra/gateways/uuid-handler.ts
@@ -1,7 +1,7 @@
 import { v4 } from 'uuid'
 import { UUIDGenerator } from '@/domain/contracts/gateways'
 
-export class UUIDHandler {
+export class UUIDHandler implements UUIDGenerator {
   uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
     return key + '_' + v4()
   }
