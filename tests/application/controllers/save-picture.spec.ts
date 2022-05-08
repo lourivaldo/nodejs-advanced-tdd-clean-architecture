a68fa8cb559bdd2fdf22bb2fc0480b2dccd5759a
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-08 20:52:36 -0300

    ensure SavePictureController returns 400 if file size is more than 5MB

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -11,12 +11,19 @@ export class InvalidMimeTypeError extends Error {
     this.name = 'InvalidMimeTypeError'
   }
 }
+export class MaxFileSizeError extends Error {
+  constructor (maxSizeInMb: number) {
+    super(`File upload limit is ${maxSizeInMb}MB`)
+    this.name = 'MaxFileSizeError'
+  }
+}
 
 class SavePictureController {
   async handle ({ file }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
     if (!['image/png', 'image/jpeg', 'image/jpg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['jpeg', 'png']))
+    if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
   }
 }
 
@@ -94,6 +101,16 @@ describe('SavePictureController', () => {
     expect(httpResponse).not.toEqual({
       statusCode: 400,
       data: new InvalidMimeTypeError(['jpeg', 'png'])
+    })
+  })
+
+  it('should return 400 if file size is bigger than 5MB', async () => {
+    const invalidBuffer = Buffer.from(new ArrayBuffer(6 * 1024 * 1024))
+    const httpResponse = await sut.handle({ file: { buffer: invalidBuffer, mimeType: 'image/jpeg' } })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new MaxFileSizeError(5)
     })
   })
 })
