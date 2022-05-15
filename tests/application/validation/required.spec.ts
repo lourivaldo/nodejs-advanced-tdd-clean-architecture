84161361178311cfda64424324f7447a0524a14e
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-14 22:15:49 -0300

    add required buffer validator

--- tests/application/validation/required.spec.ts
+++ tests/application/validation/required.spec.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError } from '@/application/errors'
-import { Required, RequiredString } from '@/application/validation'
+import { Required, RequiredBuffer, RequiredString } from '@/application/validation'
 
 describe('Required', () => {
   it('should return RequiredFieldError if value is null', () => {
@@ -44,6 +44,30 @@ describe('RequiredString', () => {
 
   it('should return undefined if value is not empty', () => {
     const sut = new RequiredString('any_value', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(undefined)
+  })
+})
+
+describe('RequiredBuffer', () => {
+  it('should extend required', () => {
+    const sut = new RequiredBuffer(Buffer.from(''))
+
+    expect(sut).toBeInstanceOf(Required)
+  })
+
+  it('should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredBuffer(Buffer.from(''))
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredBuffer(Buffer.from('any_buffer'))
 
     const error = sut.validate()
 
