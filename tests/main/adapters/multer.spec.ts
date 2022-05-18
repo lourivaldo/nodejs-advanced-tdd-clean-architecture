c23a70dd0c21aefddd8aae26952a2e2271785133
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-17 21:44:25 -0300

    ensure MulterAdapter returns 500 if upload fails

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -1,3 +1,4 @@
+import { ServerError } from '@/application/errors'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { RequestHandler, Request, Response, NextFunction } from 'express'
 import multer from 'multer'
@@ -7,7 +8,9 @@ jest.mock('multer')
 
 const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture')
-  upload(req, res, () => {})
+  upload(req, res, (error) => {
+    res.status(500).json({ error: new ServerError(error).message })
+  })
 }
 
 describe('MulterAdapter', () => {
@@ -21,7 +24,7 @@ describe('MulterAdapter', () => {
   let sut: RequestHandler
 
   beforeAll(() => {
-    uploadSpy = jest.fn()
+    uploadSpy = jest.fn().mockImplementation(() => {})
     singleSpy = jest.fn().mockImplementation(() => uploadSpy)
     multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }))
     fakeMulter = multer as jest.Mocked<typeof multer>
@@ -41,5 +44,19 @@ describe('MulterAdapter', () => {
     expect(singleSpy).toHaveBeenCalledTimes(1)
     expect(uploadSpy).toHaveBeenCalledWith(req, res, expect.any(Function))
     expect(uploadSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 500 if upload fails', async () => {
+    const error = new Error('multer_error')
+    uploadSpy.mockImplementationOnce((req, res, next) => {
+      next(error)
+    })
+
+    sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: new ServerError(error).message })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
