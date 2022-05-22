a5127f25fb01afa50f14284e35ef1578e141c12b
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-21 22:19:16 -0300

    add PgTransactionController factories

--- src/application/validation/max-file-size.ts
+++ src/application/validation/max-file-size.ts
@@ -1,6 +1,7 @@
 import { MaxFileSizeError } from '@/application/errors'
+import { Validator } from '@/application/validation/validator'
 
-export class MaxFileSize {
+export class MaxFileSize implements Validator {
   constructor (
     private readonly maxSizeInMb: number,
     private readonly value: Buffer
--- src/main/factories/application/controllers/save-picture.ts
+++ src/main/factories/application/controllers/save-picture.ts
@@ -1,6 +1,8 @@
-import { SavePictureController } from '@/application/controllers'
+import { Controller, SavePictureController } from '@/application/controllers'
+import { makePgTransactionController } from '@/main/factories/application/decorators'
 import { makeChangeProfilePicture } from '@/main/factories/domain/use-cases'
 
-export const makeSavePictureController = (): SavePictureController => {
-  return new SavePictureController(makeChangeProfilePicture())
+export const makeSavePictureController = (): Controller => {
+  const controller = new SavePictureController(makeChangeProfilePicture())
+  return makePgTransactionController(controller)
 }
