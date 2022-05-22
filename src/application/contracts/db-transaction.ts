4655479032fd471972987607ebecb7270271208c
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2022-05-21 21:46:42 -0300

    move code from tests to source

---
+++ src/application/contracts/db-transaction.ts
@@ -0,0 +1,7 @@
+
+export interface DbTransaction {
+  openTransaction: () => Promise<void>
+  closeTransaction: () => Promise<void>
+  commit: () => Promise<void>
+  rollback: () => Promise<void>
+}
--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,37 +1,8 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '@/application/helpers'
-
-export class DbTransactionController extends Controller {
-  constructor (
-    private readonly decoratee: Controller,
-    private readonly db: DbTransaction
-  ) {
-    super()
-  }
-
-  async perform (httpRequest: any): Promise<HttpResponse> {
-    await this.db.openTransaction()
-    try {
-      const httpResponse = await this.decoratee.perform(httpRequest)
-      await this.db.commit()
-      return httpResponse
-    } catch (error) {
-      await this.db.rollback()
-      throw error
-    } finally {
-      await this.db.closeTransaction()
-    }
-  }
-}
-
-interface DbTransaction {
-  openTransaction: () => Promise<void>
-  closeTransaction: () => Promise<void>
-  commit: () => Promise<void>
-  rollback: () => Promise<void>
-}
+import { DbTransaction } from '@/application/contracts'
+import { DbTransactionController } from '@/application/decorators'
 
 describe('DbTransactionController', () => {
   let db: MockProxy<DbTransaction>
